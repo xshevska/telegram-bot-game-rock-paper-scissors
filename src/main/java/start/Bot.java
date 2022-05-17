172404e38ce8066053821153d4a5a4b8157ed9d9
xshevska <xshevska@stuba.sk>
2022-05-17 13:27:03 +0200

    #add paper

--- src/main/java/start/Bot.java
+++ src/main/java/start/Bot.java
@@ -3,8 +3,12 @@ package start;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.InlineQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.InlineQueryResultArticle;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 
@@ -22,25 +26,32 @@ public class Bot {
             updates.forEach(this::process);
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
-
-
-
-
-
     }
 
     private void process(Update update) {
         //получили
         Message message = update.message();
         CallbackQuery callbackQuery = update.callbackQuery();
+        InlineQuery inlineQuery = update.inlineQuery();
 
         BaseRequest request = null; //сюда будем складывать ответ
 
-        //повзаимодействовали
-        if (message != null){
+        if (inlineQuery != null) {
+            InlineQueryResultArticle paper =
+                    new InlineQueryResultArticle("paper", "Paper", "I'm ready to fight")
+                            //сохраняем данные
+                            .replyMarkup(
+                                    new InlineKeyboardMarkup(
+                                            new InlineKeyboardButton("Loading...").callbackData("0")
+                                    )
+                            );
+
+        } else if (message != null) {
             long chatId = update.message().chat().id();
-            request = new SendMessage(chatId, "Hello!");
+            request = new SendMessage(chatId, "Hello, Jakubko!");
         }
+
+
 
         //только когда будем отвечать на события пришедшие из него
         if (request != null) {
---
+++ src/main/java/start/Starter.java
@@ -0,0 +1,7 @@
+package start;
+
+public class Starter {
+    public static void main(String[] args) {
+        new Bot().serve();
+    }
+}
